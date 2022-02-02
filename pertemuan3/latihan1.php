c331f1b197318c91c9e31d3b5f01a9ec38066f4e
dhannypramana <p.dhannypramana@gmail.com>
2022-02-02 23:40:35 +0700

    Pertemuan 3

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -1,24 +1,29 @@
-<?php 
-// Struktur Kendali /  Control FLow
-// Perulangan
-// Pengkondisian
-
-    // For
-    for ($i=0; $i < 5; $i++) { 
-        echo $i."<br>";
-    }
-
-    // While
-    $y = 0;
-    while ($y < 5) {
-        echo $y."<br>";
-        $y++;
-    }
-
-    // Do  While
-    $z = 0;
-    do {
-        echo $z."<br>";
-        $z++;
-    } while ($z < 5);
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Latihan 1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+    <!-- Templating Style -->
+        <table>
+            <?php for ($i=1; $i<=5; $i++) : ?>
+                <?php if ($i % 2 == 0) :?>
+                    <tr class="genap">
+                <?php else :?>
+                    <tr class="ganjil">
+                <?php endif;?>
+                
+                <?php for ($j=1; $j<=5; $j++) : ?>
+                    <td class="table"><?php echo $i." , "; echo $j; ?></td>
+                <?php endfor; ?>
+            </tr>
+            <?php endfor; ?>
+        </table>
+    </div>
+</body>
+</html>
