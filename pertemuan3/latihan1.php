05ecc6fb4e1c33850867bf92aaeac9452f4ec4fd
dhannypramana <p.dhannypramana@gmail.com>
2022-02-02 22:29:22 +0700

    Pertemuan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,24 @@
+<?php 
+// Struktur Kendali /  Control FLow
+// Perulangan
+// Pengkondisian
+
+    // For
+    for ($i=0; $i < 5; $i++) { 
+        echo $i."<br>";
+    }
+
+    // While
+    $y = 0;
+    while ($y < 5) {
+        echo $y."<br>";
+        $y++;
+    }
+
+    // Do  While
+    $z = 0;
+    do {
+        echo $z."<br>";
+        $z++;
+    } while ($z < 5);
+?>
