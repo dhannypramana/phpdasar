090f60359fe303eff8fa6d6be22546aed53f8150
dhannypramana <p.dhannypramana@gmail.com>
2022-02-03 00:33:38 +0700

    Pertemuan 4

---
+++ pertemuan4/date.php
@@ -0,0 +1,27 @@
+<?php 
+    // PHP build in function yang bakal sering kepake
+    // Date -> (time(), date(), mktime(), strtotime())
+
+    // Date
+    // echo date("d F Y ");
+    
+    // time
+    // UNIX Timestamp / EPOCH Time
+    // Detik yang sudah berlalu sejak 1 januari 1970 - Sekarang
+    // echo date('l, d M Y', time()+60*60*24*28);
+    // strtotime()
+    // echo date('l', strtotime('5 aug 2000'));
+
+    // String()
+    // strlen() -> String Length
+    // strcmp() -> String Comparasi
+    // explode() -> Memecah String menjadi array
+    // htmlspecialchars() -> metode request
+
+    // Utility
+    // var_dump()
+    // isset() -> apakah sebuah variabel sudah pernah dibikin atau belum (true/false)
+    // empty() -> apakah sebuah variabel kosong atau tidak (true/false)
+    // die() -> memberhentikan program
+    // sleep() -> memberhentikan sementara
+?>
