cbd7d995b7ef3940b9af01ee83a7e24e7c183707
dhannypramana <p.dhannypramana@gmail.com>
2022-02-02 20:40:43 +0700

    Pertemuan 1

---
+++ pertemuan1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Pertemuan 1 PHP Dasar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <h1><?php echo "Hello, World!"?></h1>
+    <h2>Nama saya <?php $name = "Dhanny Adhi Pramana"; echo $name;?></h2>
+</body>
+</html>
